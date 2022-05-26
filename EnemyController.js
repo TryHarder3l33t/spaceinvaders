56b5a19a608422d40039eebe2f903e1f6c1b5edf
Eric <tryharder3l33t@gmail.com>
2022-05-25 23:00:29 -0400

    night night

---
+++ EnemyController.js
@@ -0,0 +1,20 @@
+export default class Enemycontroller{
+    enemyMap = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    ]
+
+    constructor(canvas) {
+        this.canvas = canvas
+        this.createEnemies()
+    }
+    draw(ctx) { 
+    }
+    createEnemies() {
+        
+    }
+}
